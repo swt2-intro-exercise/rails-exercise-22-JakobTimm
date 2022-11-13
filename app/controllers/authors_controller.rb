5e7b48fe5b6906a854e9797659ebd1e734adbf2c
Jakob Timm <93190743+JakobTimm@users.noreply.github.com>
2022-11-13 22:15:20 +0100

    error feedback for users when creating author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,6 +4,7 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def index
@@ -13,8 +14,10 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
    
-    @author.save
-    redirect_to @author
+    if @author.save
+      redirect_to @author
+    else
+      render "new"
   end
 
   private
